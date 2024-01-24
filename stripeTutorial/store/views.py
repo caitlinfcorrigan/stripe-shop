3ae8dd1eb18d0f0d4f9cbae4b70f8ac9da0a4120
caitlincorrigan <caitlinfcorrigan@gmail.com>
2024-01-24 11:55:41 -0800

    configures payment complete webhook and view

--- stripeTutorial/store/views.py
+++ stripeTutorial/store/views.py
@@ -2,9 +2,10 @@ from typing import Any
 import stripe
 from django.shortcuts import render, redirect
 from django.conf import settings
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views import View
 from django.views.generic import TemplateView
+from django.views.decorators.csrf import csrf_exempt
 from .models import Price, Product
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
@@ -52,3 +53,31 @@ class ProductLandingPageView(TemplateView):
             "prices": prices
         })
         return context
+
+# Stripe webhooks handler (to validate payment)
+@csrf_exempt
+def stripe_webhooks(request):
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
+        )
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return HttpResponse(status=400)
+    
+    # Handle the checkout.session.completed event
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+        customer_email = session["customer_details"]["email"]
+        payment_intent = session["payment_intent"]
+
+        # TODO - send email to customer
+        
+    return HttpResponse(status=200)
