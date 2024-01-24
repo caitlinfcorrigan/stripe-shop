ad9e4c1a7bda72d8903a2297c8a5438ca1b1bb1c
caitlincorrigan <caitlinfcorrigan@gmail.com>
2024-01-24 11:19:20 -0800

    completes checkout view

--- stripeTutorial/store/admin.py
+++ stripeTutorial/store/admin.py
@@ -4,3 +4,14 @@ from .models import Price, Product
 # Register your models here.
 admin.site.register(Price)
 admin.site.register(Product)
+
+
+class PriceInlineAdmin(admin.TabularInline):
+    model = Price
+    extra = 0
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [PriceInlineAdmin]
+
+
+admin.site.register(Product, ProductAdmin)
